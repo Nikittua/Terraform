aca9aa8f6152d1225fa82fa31d03259717dfaf8e
Nikita Sergeev <nsergeev@rvision.pro>
2024-02-15 17:02:18 +0300

    refactor(tf): changed registry code decription

---
+++ instances.tf
@@ -0,0 +1,28 @@
+
+resource "openstack_compute_servergroup_v2" "servergroup" {
+  name     = "servergroup"
+  policies = ["anti-affinity"]
+}
+
+
+resource "openstack_compute_instance_v2" "instance" {
+  count = 2
+
+  name            = "${var.name}-${count.index + 1}"
+  image_id        = var.image_id
+  flavor_name     = "m2s.medium"
+  key_pair        = var.key_pair
+  security_groups = ["default"]
+
+  network {
+    uuid = openstack_networking_network_v2.network.id
+  }
+
+  scheduler_hints {
+    group = openstack_compute_servergroup_v2.servergroup.id
+  }
+
+  depends_on = [openstack_networking_subnet_v2.subnet]
+}
+
+
